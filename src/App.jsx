1ae7422b7964bed726bd2bbcb70170a2bf3a4a3d
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-12-08 13:55:21 +0300

    textTyping animation ok

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Menu from "./components/menu/Menu";
 
 function App() {
 
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen} />
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            
+          
         </div>
     )
 }
