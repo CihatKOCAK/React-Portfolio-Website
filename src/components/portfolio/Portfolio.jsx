25201250f7e618a429b61c09845254a1be8fb2bc
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-12-08 20:41:59 +0300

    work slider ok

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss'
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio } from "../../data"
 
 export default function Portfolio() {
     const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
     const list = [
         {
             id: "featured",
@@ -28,6 +30,29 @@ export default function Portfolio() {
 
     ];
 
+
+    useEffect(() => {
+        switch (selected) {
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "content":
+                setData(contentPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio)
+        }
+    }, [selected]) //apply as selected changes
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
@@ -42,26 +67,13 @@ export default function Portfolio() {
                 ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="https://www.jcgroup.hk/wp-content/uploads/2019/08/test-img-300x194_2.png" alt="Banking App" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.jcgroup.hk/wp-content/uploads/2019/08/test-img-300x194_2.png" alt="Banking App" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.jcgroup.hk/wp-content/uploads/2019/08/test-img-300x194_2.png" alt="Banking App" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.jcgroup.hk/wp-content/uploads/2019/08/test-img-300x194_2.png" alt="Banking App" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.jcgroup.hk/wp-content/uploads/2019/08/test-img-300x194_2.png" alt="Banking App" />
-                    <h3>Banking App</h3>
-                </div>
+                {data.map((d) => (
+                    <div className="item">
+                        <img src={d.img} alt="" />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
+                
             </div>
         </div>
     )
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,39 @@
 import './work.scss'
+import { worksData } from "../../data"
+import { useState } from 'react'
 
 export default function Works() {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleClick = (way) => {
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) : 
+        setCurrentSlide(currentSlide <worksData.length -1 ? currentSlide +1 :0)
+    }
+
     return (
         <div className="works" id="works">
-            
+            <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+                {worksData.map(d => (<div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={d.icon} alt="" />
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc} </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src={d.img} alt="" />
+                        </div>
+                    </div>
+                </div>))}
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClick("left")} />
+            <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClick("right")} />
         </div>
     )
 }
